cae8f7922b9f1025038434025905d8f913ec778a
piliulkinas <61793613+piliulkinas@users.noreply.github.com>
2021-02-18 09:18:30 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,38 +27,38 @@ Auth::routes();
 
 //________CLIENTS (MAIN PAGE)____________________________________________________________
 
-Route::get('/home', 'App\Http\Controllers\ClientsController@index');
+Route::get('/home', 'App\Http\Controllers\ClientsController@index')->middleware('auth');
 
-Route::post('/createNewClient', 'App\Http\Controllers\ClientsController@createClient');
+Route::post('/createNewClient', 'App\Http\Controllers\ClientsController@createClient')->middleware('auth');
 
-Route::get('/deleteClient/{id}', 'App\Http\Controllers\ClientsController@deleteClient');
+Route::get('/deleteClient/{id}', 'App\Http\Controllers\ClientsController@deleteClient')->middleware('auth');
 
-Route::get('/showClient/{id}', 'App\Http\Controllers\ClientsController@showClient');
+Route::get('/showClient/{id}', 'App\Http\Controllers\ClientsController@showClient')->middleware('auth');
 
-Route::post('/updateClient/{id}', 'App\Http\Controllers\ClientsController@updateClient');
+Route::post('/updateClient/{id}', 'App\Http\Controllers\ClientsController@updateClient')->middleware('auth');
 
-Route::post('/sendEmailToGroup', 'App\Http\Controllers\MailsController@emailToGroup');
+Route::post('/sendEmailToGroup', 'App\Http\Controllers\MailsController@emailToGroup')->middleware('auth');
 
 //________TEMPLATES______________________________________________________________________
 
-Route::get('/templates', 'App\Http\Controllers\TemplatesController@index');
+Route::get('/templates', 'App\Http\Controllers\TemplatesController@index')->middleware('auth');
 
-Route::post('/templates/createNewTemplate', 'App\Http\Controllers\TemplatesController@createTemplate');
+Route::post('/templates/createNewTemplate', 'App\Http\Controllers\TemplatesController@createTemplate')->middleware('auth');
 
-Route::get('/templates/showTemplate/{id}', 'App\Http\Controllers\TemplatesController@showTemplate');
+Route::get('/templates/showTemplate/{id}', 'App\Http\Controllers\TemplatesController@showTemplate')->middleware('auth');
 
-Route::post('/templates/updateTemplate/{id}', 'App\Http\Controllers\TemplatesController@updateTemplate');
+Route::post('/templates/updateTemplate/{id}', 'App\Http\Controllers\TemplatesController@updateTemplate')->middleware('auth');
 
-Route::get('/templates/deleteTemplate/{id}', 'App\Http\Controllers\TemplatesController@deleteTemplate');
+Route::get('/templates/deleteTemplate/{id}', 'App\Http\Controllers\TemplatesController@deleteTemplate')->middleware('auth');
 
-Route::get('/template/{id}', 'App\Http\Controllers\TemplatesController@readTemplate');
+Route::get('/template/{id}', 'App\Http\Controllers\TemplatesController@readTemplate')->middleware('auth');
 
 //________Mails_____________________________________________________________________________
 
-Route::post('/sendEmailToClient', 'App\Http\Controllers\MailsController@mailForClient');
+Route::post('/sendEmailToClient', 'App\Http\Controllers\MailsController@mailForClient')->middleware('auth');
 
-Route::post('/sendEmailToGroup', 'App\Http\Controllers\MailsController@mailForGroup');
+Route::post('/sendEmailToGroup', 'App\Http\Controllers\MailsController@mailForGroup')->middleware('auth');
 
-Route::get('/scheduledEmails', 'App\Http\Controllers\ScheduledEmailsController@index');
+Route::get('/scheduledEmails', 'App\Http\Controllers\ScheduledEmailsController@index')->middleware('auth');
 
-Route::get('/scheduledEmails/deletePlanedMail/{id}', 'App\Http\Controllers\ScheduledEmailsController@deleteScheduledEmail');
+Route::get('/scheduledEmails/deletePlanedMail/{id}', 'App\Http\Controllers\ScheduledEmailsController@deleteScheduledEmail')->middleware('auth');
